5fd48a04d2278cbc124ba027d5caea290f85f721
Russell Wallace <russell.wallace@gmail.com>
2017-03-31 16:34:07 +0100

    print

--- dimacs-parser.js
+++ dimacs-parser.js
@@ -6,6 +6,15 @@ var getStdin = require('get-stdin');
 var glob = require('glob');
 var index = require('./index');
 var os = require('os');
+var util = require('util');
+
+function print(a) {
+	console.log(util.inspect(a, {
+		depth: null,
+		maxArrayLength: null,
+		showHidden: false,
+	}));
+}
 
 // Command line
 commander.usage('[options] <files>');
@@ -25,19 +34,22 @@ if (os.platform() === 'win32') {
 		}
 	}
 }
+if (files.length === 1 && files[0] === '-') {
+	files = [];
+}
 switch (files.length) {
 case 0:
 	getStdin().then(function (text) {
-		console.log(index.parse(text, 'stdin'));
+		print(index.parse(text, 'stdin'));
 	});
 	break;
 case 1:
-	console.log(index.read(files[0]));
+	print(index.read(files[0]));
 	break;
 default:
 	for (var file of files) {
 		console.log(file);
-		console.log(index.read(file));
+		print(index.read(file));
 	}
 	break;
 }
