b539f1befd5c9840c98b0d4e910dca0c7c36ed1c
Russell Wallace <russell.wallace@gmail.com>
2017-04-26 15:37:20 +0100

    iso

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var cnf = require('clause-normal-form')
 var fs = require('fs')
 var index = require('./index')
 
-function assertEq(a, b) {
-	assert(cnf.eq(a, b))
+function assertIso(a, b) {
+	assert(cnf.isomorphic(a, b))
 }
 
 function read(file) {
@@ -16,5 +16,5 @@ it('clauses', function () {
 	var clauses = cnf.term('&')
 	clauses.push(cnf.term('|', cnf.fun('1'), cnf.term('~', cnf.fun('2'))))
 	clauses.push(cnf.term('|', cnf.term('~', cnf.fun('3')), cnf.fun('4')))
-	assertEq(read('test.cnf').clauses, clauses)
+	assertIso(read('test.cnf').clauses, clauses)
 })
