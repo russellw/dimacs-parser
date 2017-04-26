daad07a5d3341905205de4a57ce96ef3962ebef5
Russell Wallace <russell.wallace@gmail.com>
2017-04-26 13:48:45 +0100

    test

---
+++ test.js
@@ -0,0 +1,20 @@
+var assert = require('assert')
+var cnf = require('clause-normal-form')
+var fs = require('fs')
+var index = require('./index')
+
+function assertEq(a, b) {
+	assert(cnf.eq(a, b))
+}
+
+function read(file) {
+	var text = fs.readFileSync(file, 'utf8')
+	return cnf.parse(text, file)
+}
+
+it('clauses', function () {
+	var clauses = cnf.term('&')
+	clauses.push(cnf.term('|', cnf.fun('1'), cnf.term('~', cnf.fun('2'))))
+	clauses.push(cnf.term('|', cnf.term('~', cnf.fun('3')), cnf.fun('4')))
+	assertEq(read('test.cnf').clauses, clauses)
+})
